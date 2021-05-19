cc92738a80650db34117b3565b949651fc60883d
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-19 16:23:50 +0900

    Index flats created

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,45 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :delete, :update]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def show
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to restaurant_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to restaurant_path(@flat)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :url)
+  end
+end
